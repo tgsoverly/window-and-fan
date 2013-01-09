b84e20c3903a611e3a776259e8f3bc68e148e7da
Timothy Overly <timothy.overly@gmail.com>
2013-01-09 13:44:22 -0500

    initial commit of empty rails app with start of a readme file.

--- README.md
+++
@@ -1,4 +0,0 @@
-window-and-fan
-==============
-
-How to save money and stay confortable using your windows and fans.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WindowAndFan::Application.config.secret_token = '6745091a709070e1052fcf3c0faa64f815faa6f2ada96cff624acdbd2525238a5bf54a6e27bf9d41d1e5f823438ed5f94f4fec06a1eae2a341a97ed6c19934bd'
