5e4d244dd4312fcd21a00be88cb0a91e266ed6b7
Timothy Overly <tgsoverly@shuttle.(none)>
2013-01-13 07:09:46 -0500

    heroku

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -14,10 +14,10 @@ class WeatherController < ApplicationController
         weather = Weather.where("city = ? AND state = ?", params[:city], params[:state])
         if weather.any? && weather[0].valid?
           #If found and valid set it to be returned.
+          puts "found"
           weather = weather[0]
         else
           #if not the save and return a new object
-          weather = nil
           state = URI::encode(""+params[:state].to_s)
           city = URI::encode(""+params[:city].to_s)
           html = self.class.get("http://www.findlocalweather.com/hourly/#{state}/#{city}.html")
@@ -25,23 +25,26 @@ class WeatherController < ApplicationController
           case html.code
             when 200
               #Create and cache
+              puts "create and save"
               weather_data = self.parseWeatherHTML html
               weather = Weather.new(:city=>params[:city], :state=>params[:state], :time_cached=>Time.now.to_f, :weather_data=>weather_data.to_json)
               weather.save()
             when 403..404
               response_object = {'error' => 'City and state combination not found, or missing.'}
             else
+              puts "500"
               status = 500
               response_object = {'error' => 'Error getting weather data, try again later.'}
           end
         end
       else
+        puts "404"
         status = 404
         response_object = {'error' => 'City and state combination not found, or missing.'}
       end
     rescue Exception => e
+      puts "Exception"
       response_object = {'error' => "Error getting weather data, try again later."}
-      puts e.backtrace
       status = 500
     end
     if(!weather.nil? && !weather.errors.any?)
